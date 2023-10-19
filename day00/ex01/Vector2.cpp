a8118b0b891ea17d77ae9461ad67e092a1a2cc60
Marcos Chiu <marcos.chiu@gmail.com>
2023-10-19 10:04:44 -0300

    feat: dealing with negatives

--- day00/ex01/Vector2.cpp
+++ day00/ex01/Vector2.cpp
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Vector2.hpp                                        :+:      :+:    :+:   */
+/*   Vector2.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/18 10:00:02 by maalexan          #+#    #+#             */
-/*   Updated: 2023/10/19 09:59:56 by maalexan         ###   ########.fr       */
+/*   Created: 2023/10/19 09:58:30 by maalexan          #+#    #+#             */
+/*   Updated: 2023/10/19 10:01:23 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-struct Vector2
-{
-	float	x;
-	float	y;
+#include "Vector2.hpp"
+#include <stdexcept>
 
-	Vector2(float x, float y);
-};
+Vector2::Vector2(float x, float y)
+{
+	if (x < 0 || y < 0)
+		throw std::invalid_argument("Coordinates cannot be negative");
+	this->x = x;
+	this->y = y;
+}
--- day00/ex01/main.cpp
+++ day00/ex01/main.cpp
@@ -6,27 +6,34 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 14:00:12 by maalexan          #+#    #+#             */
-/*   Updated: 2023/10/18 14:58:49 by maalexan         ###   ########.fr       */
+/*   Updated: 2023/10/19 10:04:22 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Graph.hpp"
+#include <iostream>
 
 int	main(void)
 {
-	Vector2	boundries(7, 7);
+	try 
+	{
+		Vector2	boundries(7, 7);
 
-	Graph graph(boundries);
+		Graph graph(boundries);
 
-	graph.addPoint(Vector2(0, 0));
-	graph.addPoint(Vector2(2, 2));
-	graph.addPoint(Vector2(4, 2));
-	graph.addPoint(Vector2(6, 3));
-	graph.addPoint(Vector2(2, 4));
-	graph.addPoint(Vector2(4, 7));
-	graph.addPoint(Vector2(8, 7));
+		graph.addPoint(Vector2(0, 0));
+		graph.addPoint(Vector2(2, 2));
+		graph.addPoint(Vector2(4, 2));
+		graph.addPoint(Vector2(6, 3));
+		graph.addPoint(Vector2(2, 4));
+		graph.addPoint(Vector2(4, 7));
+		graph.addPoint(Vector2(8, 7));
 
-	graph.display();
-
+		graph.display();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
 	return (0);
 }
